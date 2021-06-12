54de2b0b702eb539b5f1372375ab97525b574256
Richard Silence <dune44@hotmail.com>
2021-06-12 08:24:08 -0600

    minor changes to app.js, included bodyParser limit on extended.

--- bin/app.js
+++ bin/app.js
@@ -10,18 +10,21 @@ mongoose.connect( MongoUri,  { useNewUrlParser: true, useUnifiedTopology: true }
 
 app.set( 'x-powered-by',false );
 app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded({ 'extended': false }) );
 
 app.use( helmet() );
 
 // limit user payload
-app.use( express.json({ limit: '10kb' }) );
+const limit = '10kb';
+app.use( express.json({ limit }) );
+
 // Limit user requests per router
-const limit = rateLimit({
+const limitRate = rateLimit({
     max: 100,// max requests
     windowMs: 60 * 60 * 1000, // 1 Hour
     message: 'Too many requests' // message to send
 });
-app.use( '/', limit ); // Setting limiter on specific route
+app.use( '/', limitRate ); // Setting limiter on specific route
 
 // Pull in routes
 require( './../routes/router' )( app );
